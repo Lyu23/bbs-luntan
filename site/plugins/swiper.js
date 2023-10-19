bc24f6c3b9067a36752df2296a16021ff65ac43c
Lyu23 <crackyulin@gmail.com>
2023-10-19 15:14:43 +0800

    first

---
+++ site/plugins/swiper.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+
+Vue.component('swiper', Swiper)
+Vue.component('swiper-slide', SwiperSlide)
